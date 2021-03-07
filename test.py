600e0e04447e672f9c2b337493ac6d1e7ba4c367
MasonKling21 <56097078+MasonKling21@users.noreply.github.com>
2021-03-07 15:24:26 -0800

    Added test for multiples of 5

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from fizzBuzz import *
 class TestFunction(unittest.TestCase):
     def test_fizzBuzz(self):
         self.assertEqual(fizzedBuzz(3), "Fizz")
+        self.assertEqual(fizzedBuzz(5), "Buzz")
 
 
 if __name__ == "__main__":
