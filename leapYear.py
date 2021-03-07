89b11d045770016e36f729acc533d4400dc5ba77
MasonKling21 <56097078+MasonKling21@users.noreply.github.com>
2021-03-07 15:54:32 -0800

    Added divisibility of 400 functionality

--- leapYear.py
+++ leapYear.py
@@ -1,6 +1,6 @@
 def leapYear(x):
     if (x % 4) == 0:
-        if(x % 100) == 0:
-            return("Not a leap year")
-        else:
+        if(x % 400) == 0 or (x % 100) != 0:
             return("Leap year")
+        else:
+            return("Not a leap year")
